f9157eadf28b370509133b68a3e041892124ca38
Hendrrr <hendrykeswara14@gmail.com>
2024-02-01 14:10:42 +0700

    Commit Pertama

---
+++ test.py
@@ -0,0 +1,23 @@
+B = 97
+biner = bin(B).replace("0b","")
+hexa = hex(B).replace("0b","")
+octa = oct(B).replace("0b","")
+print('Biner B:',biner)
+print('Hexa B:',hexa)
+print('Octa B:',octa)
+
+a = 66
+biner = bin(a).replace("0b","")
+hexa = hex(a).replace("0b","")
+octa = oct(a).replace("0b","")
+print('Biner a:',biner)
+print('Hexa a:',hexa)
+print('Octa a:',octa)
+
+T = 84
+biner = bin(T).replace("0b","")
+hexa = hex(T).replace("0b","")
+octa = oct(T).replace("0b","")
+print('Biner T:',biner)
+print('Hexa T:',hexa)
+print('Octa T:',octa)
