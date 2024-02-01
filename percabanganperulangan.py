8ca359afa59df377acf2d99e619be8492a23a920
Hendrrr <hendrykeswara14@gmail.com>
2024-02-01 15:58:35 +0700

    Commit Latihan Percabangan dan Perulangan

---
+++ percabanganperulangan.py
@@ -0,0 +1,39 @@
+# i = 1
+# while i < 6:
+#   print(i)
+#   i += 1
+
+# Terdapat nilai S = 12, ketika S > 18. Maka cetak 12,13,14,15 dan cetak "selesai"
+
+# s = 12
+# while s < 18:
+#     print(s)
+#     s -= 1
+# print("Selesai")
+    
+    
+# s = 12
+# while s > 0:
+#     if s == 16:
+#         break # Break Statement
+#     print(s)
+#     s += 1
+# print('Selesai')
+# while s > 15:
+#     if s == 19:
+#         break # Break Statement
+#     print('Sudah Cek: ', s)
+#     s += 1
+
+s = 12
+while s > 0:
+    if s == 16:
+        break # Break Statement
+    print(s)
+    s += 1
+while s > 15:
+    if s == 19:
+        break # Break Statement
+    print('Sudah Cek: ', s)
+    s += 1
+print('Selesai')
